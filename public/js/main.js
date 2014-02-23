42c935c54dfb89b56f8e79d669cde67eb6b5a118
PeeHaa <info@pieterhordijk.com>
2014-02-23 16:42:58 +0100

    Implemented popup JS

--- public/js/main.js
+++ public/js/main.js
@@ -26,4 +26,45 @@
             }
         });
     }
+
+    function showPopup(content) {
+        var overlay = document.createElement('div');
+        overlay.setAttribute('class', 'overlay');
+
+        var popup = document.createElement('div');
+        popup.setAttribute('class', 'popup');
+
+        popup.innerHTML = content;
+
+        var body = document.getElementsByTagName('body')[0];
+
+        body.appendChild(overlay);
+        body.appendChild(popup);
+
+        overlay.addEventListener('click', function(e) {
+            popup.parentNode.removeChild(popup);
+            overlay.parentNode.removeChild(overlay);
+        });
+    }
+
+    document.addEventListener('click', function(e) {
+        if (e.target.nodeName !== 'A' || e.target.getAttribute('href') !== '/attribution') {
+            return;
+        }
+
+        var request = new XMLHttpRequest();
+
+        request.onreadystatechange = function() {
+            if(request.readyState == 4 && request.status == 200) {
+                result = JSON.parse(request.responseText);
+
+                showPopup(result.body);
+            }
+        };
+
+        request.open('GET', e.target.getAttribute('href'), true);
+        request.send();
+
+        e.preventDefault();
+    });
 }());
